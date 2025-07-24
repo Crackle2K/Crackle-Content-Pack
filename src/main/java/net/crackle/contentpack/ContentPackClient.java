0fd4e7c9222e4fb7cdd6309933f05a09e16dd4c9
Dinesh <crackleontwitch@gmail.com>
2025-07-23 20:20:08 -0400

    Initial Commit

---
+++ src/main/java/net/crackle/contentpack/ContentPackClient.java
@@ -0,0 +1,10 @@
+package net.crackle.contentpack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ContentPackClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/crackle/contentpack/ContentPackDataGenerator.java
@@ -0,0 +1,11 @@
+package net.crackle.contentpack;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ContentPackDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
