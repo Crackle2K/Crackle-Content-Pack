92e8e1dded62a876e4342560b7c4f8a267d25121
Dinesh <crackleontwitch@gmail.com>
2025-07-24 20:38:26 -0400

    Removed Dependencies

--- src/main/java/net/crackle/contentpack/item/ModItems.java
+++ src/main/java/net/crackle/contentpack/item/ModItems.java
@@ -1,9 +1,7 @@
 package net.crackle.contentpack.item;
 
 import net.crackle.contentpack.ContentPack;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
